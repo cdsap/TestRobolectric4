6f5d01a539dc35388b9839fc676658c6596a7d0b
Inaki Villar <inaki.seri@gmail.com>
2018-08-07 14:25:22 +0700

    updating androidx and robolectric

--- app/src/test/java/com/inaki/robolectric4/MainActivityTest.kt
+++ app/src/test/java/com/inaki/robolectric4/MainActivityTest.kt
@@ -1,11 +1,12 @@
 package com.inaki.robolectric4
 
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.withId
-import android.support.test.espresso.matcher.ViewMatchers.withText
-import android.support.test.rule.ActivityTestRule
-import android.support.test.runner.AndroidJUnit4
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
